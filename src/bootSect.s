ee0a14f184f33acbcec15c2085e53d6e00d678d8
NeoGenio <neogenio7@gmail.com>
2021-08-17 16:10:21 +0530

    Playing with Interrupts

--- src/bootSect.s
+++ src/bootSect.s
@@ -1,5 +1,39 @@
-jmp $
+org 0x7c00
 
-times 510-($-$$) db 0
+mov ah, 0x00
+mov al, 0x03
+int 0x10
 
-dw 0xaa55
+mov ah, 0x0b
+mov bh, 0x00
+mov bl, 0x01
+int 0x10
+
+mov ah, 0x0e
+
+mov bx, string
+call print
+
+mov bx, string2
+call print
+
+print:
+    mov al, [bx]
+    cmp al, 0
+    je print_end
+    int 0x10
+    add bx, 1
+    jmp print
+
+print_end:
+    ret
+
+string: db 'This is a string!', 0xa, 0xd, 0
+string2: db 'This is also a string.', 0
+
+end:
+    jmp $
+
+    times 510-($-$$) db 0
+
+    dw 0xaa55
